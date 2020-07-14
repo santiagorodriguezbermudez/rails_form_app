f10f850f2aeadb86a0770164480bab261076e702
mikethreels <michaelthreels@gmail.com>
2020-07-14 22:33:36 +0200

    added error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
         if @user.save
           redirect_to new_user_path
         else
-          render :new
+          render new_user_path
         end
     end
 
@@ -19,8 +19,11 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find(params[:id])
-        @user.update(user_params)
-        redirect_to new_user_path
+        if @user.update(user_params)
+            redirect_to edit_user_path
+        else
+            render :edit
+        end
     end
 
     private 
