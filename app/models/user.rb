d66ac14de99294923c02e1ad12ef18b828e760f0
mikethreels <michaelthreels@gmail.com>
2020-07-14 18:40:55 +0200

    added user model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    validates :username, presence: {message: "username can't be empty"} 
+    validates :email, presence: {message: "username can't be empty"},
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Please enter a valid email' } 
+    validates :password, presence: {message: "username can't be empty"},
+    length: {minimum: 6, message: "password has to have a minimum of 6 characters"}
+end
